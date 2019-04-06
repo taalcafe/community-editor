aeb1eb7d9db456b6ed04b1f9acdd1866b0bd6341
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 01:52:28 +0200

    dev

--- src/app/translation-files/components/translation-files/translation-files.component.ts
+++ src/app/translation-files/components/translation-files/translation-files.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-translation-files',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TranslationFilesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onBack() {
+    this.router.navigate(['applications']);
   }
 
 }
--- src/app/translation-files/translation-files.module.ts
+++ src/app/translation-files/translation-files.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslationFilesComponent } from './components/translation-files/translation-files.component';
 import { RouterModule } from '@angular/router';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [TranslationFilesComponent],
   imports: [
     CommonModule,
+    NgZorroAntdModule,
 
     RouterModule.forChild([
       {
