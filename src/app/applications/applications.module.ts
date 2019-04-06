23b7e97d12f4c7fa4fbae043add89e0058af6274
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 00:21:47 +0200

    dev

--- src/app/applications/applications.module.ts
+++ src/app/applications/applications.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApplicationsComponent } from './containers/applications/applications.component';
 import { RouterModule } from '@angular/router';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [ApplicationsComponent],
   imports: [
     CommonModule,
+    NgZorroAntdModule,
 
     RouterModule.forChild([
       {
