6b17210ba8020ef264a4798cdf9cead51bfe6452
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 01:14:46 +0200

    dev

--- src/app/applications/containers/applications/applications.component.ts
+++ src/app/applications/containers/applications/applications.component.ts
@@ -12,7 +12,8 @@ export class ApplicationsComponent implements OnInit {
     { name: 'Instructor Marketplace', updatedAt: '2019-04-06T22:36:40.937Z'},
     { name: 'Product Marketplace', updatedAt: '2019-04-04T20:15:43.937Z'},
     { name: 'Branded Portal', updatedAt: '2019-04-01T17:10:40.937Z'},
-    { name: 'Sahara', updatedAt: '2019-03-28T14:00:40.937Z'}
+    { name: 'Sahara', updatedAt: '2019-03-28T14:00:40.937Z'},
+    { name: 'Edcast iOS Application', updatedAt: '2019-03-24T10:07:40.937Z'}
   ]
 
   constructor() { }
