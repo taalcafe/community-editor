20ff17ff9756634e6163b455a412fb2761187654
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 21:06:56 +0200

    dev

--- src/app/translations/containers/translations/translations.component.ts
+++ src/app/translations/containers/translations/translations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-translations',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TranslationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onBack() {
+    this.router.navigate(['translation-files', 1]);
   }
 
 }
