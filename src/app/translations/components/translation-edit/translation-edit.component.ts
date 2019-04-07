8a870fcea02b4a255abfecd1ca71b503cdf71eee
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 23:25:37 +0200

    dev

---
+++ src/app/translations/components/translation-edit/translation-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-translation-edit',
+  templateUrl: './translation-edit.component.html',
+  styleUrls: ['./translation-edit.component.less']
+})
+export class TranslationEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/translations/translations.module.ts
+++ src/app/translations/translations.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TranslationsComponent } from './containers/translations/translations.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { RouterModule } from '@angular/router';
+import { TranslationEditComponent } from './components/translation-edit/translation-edit.component';
 
 @NgModule({
-  declarations: [TranslationsComponent],
+  declarations: [TranslationsComponent, TranslationEditComponent],
   imports: [
     CommonModule,
     NgZorroAntdModule,
