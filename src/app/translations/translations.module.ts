4b5d280342a0251ce9760a40e41a92025031522b
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 13:44:23 +0200

    dev

---
+++ src/app/translations/translations.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslationsComponent } from './containers/translations/translations.component';
+
+@NgModule({
+  declarations: [TranslationsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TranslationsModule { }
