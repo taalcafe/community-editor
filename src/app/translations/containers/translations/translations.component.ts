506783bbda6400d6ddd3c0de73cbd29842f406ae
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-04-07 22:19:41 +0200

    dev

--- src/app/translations/containers/translations/translations.component.ts
+++ src/app/translations/containers/translations/translations.component.ts
@@ -8,6 +8,44 @@ import { Router } from '@angular/router';
 })
 export class TranslationsComponent implements OnInit {
 
+  showComments: boolean;
+  showNotes: boolean;
+
+  comments = [
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: new Date()
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: new Date()
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: new Date()
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: new Date()
+    }
+  ];
+
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -15,6 +53,22 @@ export class TranslationsComponent implements OnInit {
 
   onBack() {
     this.router.navigate(['translation-files', 1]);
+  }
+
+  openComments() {
+    this.showComments = true;
+  }
+
+  openNotes() {
+    this.showNotes = true;
+  }
+
+  closeComments() {
+    this.showComments = false;
+  }
+
+  closeNotes() {
+    this.showNotes = false;
   }
 
 }
