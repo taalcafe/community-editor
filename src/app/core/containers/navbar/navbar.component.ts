776b7ff97052cebfc6c6d1730f65c4f8dea286fa
Dimi <dimi@Dimis-MacBook-Pro.local>
2019-09-07 01:18:36 +0200

    dev: github button

--- src/app/core/containers/navbar/navbar.component.ts
+++ src/app/core/containers/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigurationsService } from 'src/app/shared/services/configurations.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private configurations: ConfigurationsService) { }
 
   ngOnInit() {
+  }
+
+  goToGitHub() {
+    window.open(this.configurations.gitHubUrl, '_blank');
   }
 
 }
